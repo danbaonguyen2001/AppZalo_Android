d267cc1c10bf2632d11b13516db4a2dbd4a72581
Nguyen <danbaonguyen2001@gmail.com>
2022-05-24 18:41:29 +0700

    Nguyen

---
+++ app/src/main/java/hcmute/danbaonguyen19110036/appzalo/ChatboxActivity.java
@@ -0,0 +1,14 @@
+package hcmute.danbaonguyen19110036.appzalo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatboxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbox);
+    }
+}
