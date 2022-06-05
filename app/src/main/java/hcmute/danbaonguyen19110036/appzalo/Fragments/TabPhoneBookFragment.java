1d09cf1ef82a49160a8d4e110fa963a42db358a7
Nguyen <danbaonguyen2001@gmail.com>
2022-06-05 20:02:34 +0700

    nguyen

--- app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Fragments/TabPhoneBookFragment.java
+++ app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Fragments/TabPhoneBookFragment.java
@@ -25,6 +25,6 @@ public class TabPhoneBookFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_phone_book, container, false);
+        return inflater.inflate(R.layout.fragment_main_tab_phonebook, container, false);
     }
 }
