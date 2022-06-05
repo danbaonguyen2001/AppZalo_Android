9d5eb8f1b85a02f44034182527dd0aabd3d98cb6
giangnguyen3246@mail.com <giangnguyen3246@mail.com>
2022-06-05 10:42:19 +0700

    update

--- app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Activities/SplashActivity.java
+++ app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Activities/SplashActivity.java
@@ -1,19 +1,43 @@
 package hcmute.danbaonguyen19110036.appzalo.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import hcmute.danbaonguyen19110036.appzalo.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private String imageToken;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    private Uri imagepath;
+    private static int PICK_IMAGE=123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +60,74 @@ public class SplashActivity extends AppCompatActivity {
                 }
             }
         },2000);
+    }
+    private void ConnectFirebase(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference=firebaseStorage.getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+    }
+    private void sendImageToStore(){
+        StorageReference imgref = storageReference.child("Images").child(firebaseAuth.getUid()).child("Profile Pic");;
+        Bitmap bitmap=null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imagepath);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,25,byteArrayOutputStream);
+        byte[] data=byteArrayOutputStream.toByteArray();
+        UploadTask uploadTask=imgref.putBytes(data);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imgref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        imageToken=uri.toString();
+                        Toast.makeText(getApplicationContext(),"URI get sucess",Toast.LENGTH_SHORT).show();
+                        sendDataTocloudFirestore();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),"URI get Failed",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                Toast.makeText(getApplicationContext(),"Image is uploaded",Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Image Not UPdloaded",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void sendDataTocloudFirestore(){
+        DocumentReference documentReference = firebaseFirestore.collection("Users").document(firebaseAuth.getUid());
+        Map<String,Object> userdata = new HashMap<>();
+        userdata.put("image",imageToken);
+        userdata.put("uid",firebaseAuth.getUid());
+        userdata.put("status","Online");
+        documentReference.set(userdata).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Toast.makeText(getApplicationContext(),"Data on Cloud Firestore send success",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode==PICK_IMAGE && resultCode==RESULT_OK)
+        {
+            imagepath=data.getData();
+            System.out.println("onActivityResult :"+imagepath);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
