883e02c523d98d7989f59398f1c5a94923edb132
Nguyen <danbaonguyen2001@gmail.com>
2022-06-06 23:01:55 +0700

    nguyen

--- app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Activities/FriendRequestActivity.java
+++ app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Activities/FriendRequestActivity.java
@@ -1,14 +1,31 @@
 package hcmute.danbaonguyen19110036.appzalo.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import hcmute.danbaonguyen19110036.appzalo.Adapter.FriendRequestAdapter;
+import hcmute.danbaonguyen19110036.appzalo.Fragments.FriendRequestFragment;
+import hcmute.danbaonguyen19110036.appzalo.R;
+
 public class FriendRequestActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_request);
+
+        tabLayout=findViewById(R.id.tab_layout);
+        viewPager=findViewById(R.id.view_pager);
+        FriendRequestAdapter friendRequestAdapter= new FriendRequestAdapter(getSupportFragmentManager(),
+                FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(friendRequestAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Adapter/FriendRequestAdapter.java
+++ app/src/main/java/hcmute/danbaonguyen19110036/appzalo/Adapter/FriendRequestAdapter.java
@@ -1,2 +1,43 @@
-package hcmute.danbaonguyen19110036.appzalo.Adapter;public class FriendRequestAdapter {
+package hcmute.danbaonguyen19110036.appzalo.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import hcmute.danbaonguyen19110036.appzalo.Fragments.FriendRequestFragment;
+
+public class FriendRequestAdapter extends FragmentStatePagerAdapter {
+
+    public FriendRequestAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return new FriendRequestFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title="";
+        switch (position){
+            case 0:
+                title="Đã nhận";
+                break;
+            case 1:
+                title="Đã gửi";
+                break;
+        }
+        return title;
+    }
 }
